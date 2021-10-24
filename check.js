f4b79a680dae18eec1a19afa1185d013ca782e56
Ak1r4 Yuk1 <72250044+Akira96kill@users.noreply.github.com>
2021-10-25 01:54:26 +0200

    Update check.js

--- check.js
+++ check.js
@@ -2,7 +2,8 @@ var check = function() {
   if (document.getElementById('password').value == "31102021") {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'Password Corretta';
-    document.getElementById('message').innerHTML = '#NiceTry# || https://akirayuki.it/level3.html';
+    document.getElementById('message').innerHTML = '#NiceTry#';
+    document.getElementById('message2').innerHTML = "https://akirayuki.it/level3.html";
   } else {
 
   }
