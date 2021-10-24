e8af0783fa88006da5fd6c661106d2d057da2714
Ak1r4 Yuk1 <72250044+Akira96kill@users.noreply.github.com>
2021-10-25 01:33:02 +0200

    Create check.js

---
+++ check.js
@@ -0,0 +1,9 @@
+var check = function() {
+  if (document.getElementById('password').value == "31102021") {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'Password Corretta';
+    document.getElementById('message').innerHTML = '#NiceTry#';
+  } else {
+
+  }
+}
