d067883474630b5938264cc57c7404b7ff3b2241
Ak1r4 Yuk1 <72250044+Akira96kill@users.noreply.github.com>
2021-10-25 14:19:12 +0200

    Update check.js

--- check.js
+++ check.js
@@ -1,13 +1,16 @@
 var check = function() {
-  if (document.getElementById('password').value == "31102021") {
-    document.getElementById('message').style.color = 'green';
-    document.getElementById('message').innerHTML = '#NiceTry#';
-    document.getElementById("hidden").style.visibility = "visible";
-  } 
-  else {
-   }
+        if (document.getElementById('password').value == "31102021") {
+            document.getElementById('message').style.color = 'green';
+            document.getElementById('message').innerHTML = '#NiceTry#';
+            x = document.getElementById('hidden');
+	      if (x){
+              if (x.style.display !== "none") {
+                x.style.display = "block";
+              } else {
+                  x.style.display = "none";
+                }}
+        }
 }
-
 var next = function() {
-  window.location.href = "letsbegin.html";
+	header.location('letsbegin.html')
 }
