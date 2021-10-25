8b7224874acac7a93338b7fa8811cf51f63a75ca
Ak1r4 Yuk1 <72250044+Akira96kill@users.noreply.github.com>
2021-10-25 13:30:12 +0200

    Update check.js

--- check.js
+++ check.js
@@ -2,7 +2,7 @@ var check = function() {
   if (document.getElementById('password').value == "31102021") {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = '#NiceTry#';
-  
+    next();
   } else {
 
   }
