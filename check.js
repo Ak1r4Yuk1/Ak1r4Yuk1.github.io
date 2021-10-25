88fd8b8e595bf91558a58f312a5d351bcbf86456
Ak1r4 Yuk1 <72250044+Akira96kill@users.noreply.github.com>
2021-10-25 13:36:24 +0200

    Update check.js

--- check.js
+++ check.js
@@ -2,12 +2,12 @@ var check = function() {
   if (document.getElementById('password').value == "31102021") {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = '#NiceTry#';
-    next();
+    document.getElementById("hidden").style.visibility = "visible";
   } 
   else {
    }
 }
 
 var next = function() {
-  document.getElementById("hidden").style.visibility = "visible";
+  
 }
